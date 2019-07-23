6f90a2dee80e7a87ee70ec76b5a31aef669f8eb8
carlludwi <carl.ludwi@gmail.com>
2019-07-23 19:15:45 +0700

    footer2307

--- src/shared/Home/Home.tsx
+++ src/shared/Home/Home.tsx
@@ -15,18 +15,50 @@ export default class Home extends React.Component{
                 </div>
                 <div className="bottom-contents">
                     <div className="grid-wrapper">
-                        <div className="col-md-3 small-card"><div className="small-card-content">asd</div></div>
-                        <div className="col-md-3 small-card"><div className="small-card-content">asd</div></div>
-                        <div className="col-md-3 small-card"><div className="small-card-content">asd</div></div>
-                        <div className="col-md-3 small-card"><div className="small-card-content">asd</div></div>
-                        <div className="col-md-12 big-card"><div className="big-card-content">panjang</div></div>
-                        <div className="slider-container">
+                        <div className="explore-grid">
+                            <h3 className="explore-title"><b>Explore Aivbnb</b></h3>
+                            <div className="col-md-3 xsmall-card">
+                                <div className="xsmall-card-content">
+                                    <div className="xsmall-photo xs-photo xs-p-a"></div>
+                                    <div className="xsmall-photo-description">asd</div>
+                                </div>
+                            </div>
+                            <div className="col-md-3 xsmall-card">
+                                <div className="xsmall-card-content">
+                                    <div className="xsmall-photo xs-photo xs-p-b"></div>
+                                    <div className="xsmall-photo-description">asd</div>
+                                </div>
+                            </div>
+                            <div className="col-md-3 xsmall-card">
+                                <div className="xsmall-card-content">
+                                    <div className="xsmall-photo xs-photo xs-p-c"></div>
+                                    <div className="xsmall-photo-description">asd</div>
+                                </div>
+                            </div>
+                            <div className="col-md-3 xsmall-card">
+                                <div className="xsmall-card-content">
+                                    <div className="xsmall-photo xs-photo xs-p-d"></div>
+                                    <div className="xsmall-photo-description">asd</div>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="luxe-grid">
+                            <h3 className="luxe-title"><b>Introducing Aivbnb Luxe</b></h3>
+                            <div className="col-md-12 big-card">
+                                <div className="big-card-content">
+                                    <div></div>
+                                    <div></div>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="recommendation-grid">
+                            <h3 className="recommendation-title"><b>Recommendation for you</b></h3>
                             <div className="col-md-2 medium-card a">slidera</div>
                             <div className="col-md-2 medium-card b">sliderb</div>
                             <div className="col-md-2 medium-card c">sliderc</div>
                             <div className="col-md-2 medium-card d">sliderd</div>
                             <div className="col-md-2 medium-card e">slidere</div> 
-                            <div className="col-md-2 medium-card f">sliderf</div>                        
+                            <div className="col-md-2 medium-card f">sliderf</div> 
                         </div>
                     </div>
                 </div>
